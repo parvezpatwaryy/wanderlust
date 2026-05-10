b48bc8b8c6684e85d4cabf03294e63192c69595a
parvezpatwaryy <tanjilapatwaryy@gmail.com>
2026-05-11 00:42:58 +0600

    creacte nextjs project and add heroui

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/'}>Home</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
