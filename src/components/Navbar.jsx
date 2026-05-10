bb69fa4f63378058b08505674dde0099dffd27b4
parvezpatwaryy <tanjilapatwaryy@gmail.com>
2026-05-11 01:17:16 +0600

    navbar banner and footer section complite

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,27 @@
 import Link from 'next/link';
 import React from 'react';
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
+    <nav className='flex justify-between p-5 bg-white'>
+      <ul className='flex gap-3 justify-center items-center'>
         <li><Link href={'/'}>Home</Link></li>
-        <li><Link href={'/'}>Home</Link></li>
-        <li><Link href={'/'}>Home</Link></li>
-        <li><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/Destinations'}>Destinations</Link></li>
+        <li><Link href={'/My-Bookings'}>My Bookings</Link></li>
+      </ul>
+      <div>
+        <Image
+        src={'/assets/Wanderlast.png'}
+        alt="logo"
+        height={150}
+        width={150}
+        />
+      </div>
+      <ul className='flex gap-3 justify-center items-center'>
+        <li><Link href={'/Profile'}>Profile</Link></li>
+        <li><Link href={'/Login'}>Login</Link></li>
+        <li><Link href={'/SignUp'}>Sign Up</Link></li>
       </ul>
     </nav>
   );
